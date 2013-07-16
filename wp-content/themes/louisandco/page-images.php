79ab4e74a741b9d32744356b26ed6aa13eae76da
Michael <michael@falsebehavinganimals.com>
2013-07-16 10:16:36 +0200

    updated
    Signed-off-by: Michael <michael@falsebehavinganimals.com>

--- wp-content/themes/louisandco/page-images.php
+++ wp-content/themes/louisandco/page-images.php
@@ -12,18 +12,19 @@
 ?>
 <?php Starkers_Utilities::get_template_parts( array( 'parts/shared/html-header', 'parts/shared/header' ) ); ?>
 
+
+
+
 <div class="packery">
 
 <?php
 $rows = get_field('image_gallery_lco');
 if($rows){ ?>
-	<ul>
  <?php foreach($rows as $row){?>
 		<div class="itemPack" data-tag="<?php forEach($row['sub_taxonomy'] as $a){echo $a.' '; }; ?>">
   		<img  src="<?php echo $row['sub_images'] ?>" />
   	</div>
 	<?php } ?>
-	</ul>
 <?php } ?>
 
 <?php Starkers_Utilities::get_template_parts( array( 'parts/shared/footer','parts/shared/html-footer' ) ); ?>
